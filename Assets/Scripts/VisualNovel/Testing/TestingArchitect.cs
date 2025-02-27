c2c39c08427ffbe9fd3bbc8d7902d3433ab859e3
Guerreroe300 <guerreroe200@yahoo.com>
2025-02-27 13:39:54 -0600

    Implementacion base de Dialogue System

---
+++ Assets/Scripts/VisualNovel/Testing/TestingArchitect.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TESTING{
+    public class TestingArchitect : MonoBehaviour
+    {
+        DialogueSystem ds;
+        TextArchitect architect;
+
+        string[] lines = new string[5]{
+            "BRO WE GOT DIALOGUE",
+            "GAHHHHHHHHHHHH",
+            "BRUH",
+            "ojfkljsfkjnvjnvlijdnbljnbldjngbijdnbv",
+            "===================================================================================================================="
+        };
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            ds = DialogueSystem.instance;
+            architect = new TextArchitect(ds.dialogueContainer.dialogueText);
+            architect.buildMethod = TextArchitect.BuildMethod.typewriter;
+            architect.speed = 0.5f;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            string longLine = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
+            if (Input.GetKeyDown(KeyCode.Space)){
+                if(architect.isBuilding){
+                    if(!architect.hurryUp){
+                        architect.hurryUp = true;
+                    }
+                    else{
+                        architect.ForceComplete();
+                    }
+                }
+                else{
+                    //architect.Build(lines[Random.Range(0, lines.Length)]);
+                    architect.Build(longLine);
+                }
+            }
+            else if (Input.GetKeyDown(KeyCode.A)){
+                //architect.Append(lines[Random.Range(0, lines.Length)]);
+                architect.Append(longLine);
+            }
+        }
+    }
+}// a
